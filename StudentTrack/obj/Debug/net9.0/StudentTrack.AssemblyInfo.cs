0efdbfb875156c22db12798b6f008846654c7cbd
AntonioPlaza7 <antoniojoseplacu@gmail.com>
2025-05-28 19:40:09 +0200

    Portables con publishSingleFile para una version mas reducida

--- StudentTrack/obj/Debug/net9.0/StudentTrack.AssemblyInfo.cs
+++ StudentTrack/obj/Debug/net9.0/StudentTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a211a9c5e64da717c76cc171ef0fe27e7947114f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd3ebf1f4037f1c06d6fbc9155e91aa99d59034")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
